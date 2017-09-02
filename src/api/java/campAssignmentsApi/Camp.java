b75736653de90383790835ec9bb4dea2a3017aee
Bert J Zylstra <bert.zylstra65@gmail.com>
2017-09-02 16:53:07 -0600

    Initial commit
    Started from API demo project and trimmed down to just API and tests
    Added Camp.java

---
+++ src/api/java/campAssignmentsApi/Camp.java
@@ -0,0 +1,55 @@
+package campAssignmentsApi;
+
+import java.util.Date;
+
+/**
+ * @author Bert
+ * 
+ * Public API version of a Camp object.
+ */
+public class Camp {
+    private long campId;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    
+    public Camp(long id, String description, Date startDate, Date endDate) {
+    	setId(id);
+    	setDescription(description);
+    	setStartDate(startDate);
+    	setEndDate(endDate);
+    }
+    
+    public long getId() {
+    	return campId;
+    }
+    
+    public void setId(long id) {
+    	this.campId = id;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+    	this.description = description; 
+    }
+
+    public Date getStartDate() {
+    	return startDate;
+    }
+    
+    public void setStartDate(Date startDate) {
+    	this.startDate = startDate;
+    }
+    
+    public Date getEndDate() {
+    	return endDate;
+    }
+    
+    public void setEndDate(Date endDate) {
+    	this.endDate = endDate;
+    }
+
+}
